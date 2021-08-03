8415f817ba6e59205ddd8d13e6e3d386e1c06355
Alexey426 <khytov@gmail.com>
2021-08-03 14:15:48 +0300

    Initial commit

---
+++ src/Arithmetic.java
@@ -0,0 +1,27 @@
+import java.sql.SQLOutput;
+
+public class Arithmetic {
+
+      int oneNumber, twoNumber;
+
+
+      Arithmetic() {
+             oneNumber = 22;
+             twoNumber = 55;
+         }
+         Arithmetic(int o, int t){
+            o = oneNumber;
+            t = twoNumber;
+
+
+
+
+         }
+
+
+}
+
+
+
+
+
